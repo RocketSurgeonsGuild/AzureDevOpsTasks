c073863160138d4f5fb5246e9c6b629636a2ab9f
David Driscoll <david.driscoll@gmail.com>
2018-10-07 14:40:17 -0400

    Don't publish artifacts by default from serialize variables

--- tasks/SerializeVariables/task.ts
+++ tasks/SerializeVariables/task.ts
@@ -1,36 +1,41 @@
-import { mkdir, writeFile } from 'fs';
-import { join } from 'path';
+import { dirname } from 'path';
 import { bindNodeCallback } from 'rxjs';
-import { command, getInput, getVariable, getVariables } from 'vsts-task-lib/task';
+import { getPathInput, mkdirP, writeFile } from 'vsts-task-lib';
+import { command, getBoolInput, getInput, getVariable, getVariables } from 'vsts-task-lib/task';
 import { Task } from './util/Task';
 import { VariableHelper } from './util/VariableHelper';
 
 class SerializeVariables extends Task {
     protected async run() {
-        const filenameValue = getInput('filename', true);
-        const artifactName = getInput('artifactName', true);
+        const filenameValue = getPathInput('filename', true);
+        const publishArtifact = getBoolInput('publishArtifact', true);
         const prefixValue = getInput('prefixes', true);
+
         const prefixes = prefixValue.toLowerCase().split(',');
         const variables = getVariables()
             .filter(x => !x.secret)
             .filter(x => prefixes.some(prefix => x.name.toLowerCase().startsWith(prefix)));
 
-        const artifactDirectory = this.variable.getVariable('Build.ArtifactStagingDirectory');
-        const folder = artifactName.replace(/\{prefixes\}/gi, prefixValue.split(',').join('-'));
-        const metaPath = join(artifactDirectory, folder);
-        const metaFilePath = join(metaPath, filenameValue);
+        const metaFilePath = filenameValue.replace(/\{prefixes\}/gi, prefixValue.split(',').join('-'));
+        const metaPath = dirname(metaFilePath);
 
-        await bindNodeCallback(mkdir)(metaPath).toPromise();
-        await bindNodeCallback(writeFile)(metaFilePath, JSON.stringify(variables)).toPromise();
+        mkdirP(metaPath);
+        writeFile(metaFilePath, JSON.stringify(variables));
 
-        const data = {
-            artifacttype: 'container',
-            artifactname: folder,
-            containerfolder: folder,
-            localpath: metaPath,
-        };
+        if (publishArtifact) {
+            const artifactName = getInput('artifactName', true).replace(
+                /\{prefixes\}/gi,
+                prefixValue.split(',').join('-')
+            );
+            const data = {
+                artifacttype: 'container',
+                artifactname: artifactName,
+                containerfolder: artifactName,
+                localpath: metaPath,
+            };
 
-        command('artifact.upload', data, metaPath);
+            command('artifact.upload', data, metaPath);
+        }
     }
 }
 
